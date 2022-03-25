1cf87d4b0961646564fa0c64558af9a3d314a8dd
David Hamran <TheDavid561@gmail.com>
2022-03-25 16:14:56 +0100

    CV08

---
+++ Exercises/CV08/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cviceni7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathWrite = @"writerTeploty.txt";
+            string pathRead = @"readerTeploty.txt";
+
+            ArchivTeplot teploty = new ArchivTeplot();
+
+            teploty.Load(pathRead);
+            teploty.TiskTeplot();
+            teploty.TiskPrumernychTeplot();
+            teploty.TiskPrumernychMesicnichTeplot();
+
+            teploty.Kalibrace(-0.1);
+            teploty.Vyhledej(2018);
+
+            teploty.Save(pathWrite);
+        }
+    }
+}
---
+++ Exercises/CV08/RocniTeplota.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cviceni7
+{
+    class RocniTeplota
+    {
+        private double rok;
+        private List<double> mesicniTeploty;
+        private double maxTeplota;
+        private double minTeplota;
+        private double prumernaRocniTeplota;
+
+        public RocniTeplota(double rok, List<double> teploty)
+        {
+            this.rok = rok;
+            this.mesicniTeploty = new List<double>();
+            this.mesicniTeploty = teploty;
+        }
+
+        public int pocet()
+        {
+            return mesicniTeploty.Count;
+        }
+
+        public double Rok
+        {
+            get { return rok; }
+            set { rok = value; }
+        }
+
+        public List<double> MesicniTeploty
+        {
+            get=> mesicniTeploty;
+            set=> mesicniTeploty = value; 
+        }
+        
+        public double MaxTeplota
+        {
+            get 
+            {
+                maxTeplota = mesicniTeploty[0];
+                foreach (double teplota in mesicniTeploty)
+                {
+                    if (teplota > maxTeplota)
+                    {
+                        maxTeplota = teplota;
+                    }
+                }
+                return maxTeplota; 
+            }
+        }
+
+        public double MinTeplota
+        {
+            get 
+            {
+                minTeplota = mesicniTeploty[0];
+                foreach (double teplota in mesicniTeploty)
+                {
+                    if (teplota < minTeplota)
+                    {
+                        minTeplota = teplota;
+                    }
+                }
+                return minTeplota; 
+            }
+        }
+
+        public double PrumernaRocniTeplota
+        {
+            get 
+            {
+                prumernaRocniTeplota = mesicniTeploty.Average();
+                return prumernaRocniTeplota; 
+            }
+        }
+
+    }
+}
