7b4401809a73fcb4255761ed0e2f6d0362c74705
David Hamran <TheDavid561@gmail.com>
2022-04-12 10:35:24 +0200

    CV10

---
+++ Exercises/CV10/SQLQuery4.sql
@@ -0,0 +1,35 @@
+﻿
+SELECT * FROM Student
+SELECT * FROM Predmet
+SELECT * FROM Hodnoceni
+
+Select * from Spojovacia
+
+
+SELECT * 
+FROM Student 
+LEFT JOIN Spojovacia ON Student.id_student = Spojovacia.IdStudent
+
+
+
+SELECT lastName, 
+count(id_Student) as studentCount 
+FROM Student group by lastName  
+order by studentCount DESC
+
+
+
+SELECT SubjectACR 
+FROM Spojovacia 
+group by SubjectACR
+having count(IdStudent)<3
+
+
+
+SELECT name_subject_rating,  
+max(Rating) as highScore, 
+min(Rating) as lowScore,
+avg(Rating) as averageScore,
+count(IdStudent_rating) as countScore
+FROM Hodnoceni group by
+name_subject_rating
