85b60b623c8df1beb5ef7764eebc80139cd1fded
David Hamran <TheDavid561@gmail.com>
2022-05-02 18:19:28 +0200

    update

---
+++ Exercises/CV10/dbo.Spojovacia.data.sql
@@ -0,0 +1,4 @@
+﻿INSERT INTO [dbo].[Spojovacia] ([SubjectACR], [IdStudent]) VALUES (N'BPC-MA2   ', 230907)
+INSERT INTO [dbo].[Spojovacia] ([SubjectACR], [IdStudent]) VALUES (N'BPC-OOP   ', 0)
+INSERT INTO [dbo].[Spojovacia] ([SubjectACR], [IdStudent]) VALUES (N'BPC-OOP   ', 230000)
+INSERT INTO [dbo].[Spojovacia] ([SubjectACR], [IdStudent]) VALUES (N'BPC-OOP   ', 230253)
